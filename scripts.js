d1d8aca424c3c857e10abd982ec0237603c85e41
Rodalife <hiago.rafael.souza@hotmail.com>
2020-07-14 10:36:27 -0300

    EventListener

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.querySelector('#button-menu').addEventListener('click',function(){
+  menuBox = document.querySelector("#navmenu");
+  if(menuBox.style.display == "block") { 
+    menuBox.style.display = "none";
+  }
+  else { 
+    menuBox.style.display = "block";
+  }
+
+}) 
+   
+  
+  
+  
